da1770cbdec72285dbcd40b8325a4ade2bbb7816
Lee Kin Fei <kinfei.lee@qumonintelligence.com>
2021-10-05 21:05:33 +0800

    Init

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+// const bodyParser = require("body-parser"); /* deprecated */
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+const db = require("./models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to tutorial application." });
+});
+
+require("./routes/tutorial.routes")(app);
+
+app.listen(3000, () => console.log(`server has started at port 3000`));
